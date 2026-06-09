39469e1a835d630690ecb851309a5ced0545b130
Zubokryl <zubokryl@gmail.com>
2026-06-09 13:08:55 +0200

    Update design

--- prorok/assets/js/landing.js
+++ prorok/assets/js/landing.js
@@ -7,7 +7,7 @@
     const rect = scene.getBoundingClientRect();
     const x = (e.clientX - rect.left) / rect.width - 0.5;
     const y = (e.clientY - rect.top) / rect.height - 0.5;
-    bg.style.transform = 'scale(1.06) translate(' + (x * -12) + 'px, ' + (y * -12) + 'px)';
+    bg.style.transform = 'translate(' + (x * -6) + 'px, ' + (y * -6) + 'px)';
   });
 
   scene.addEventListener('mouseleave', function () {
